63d1016c3270ad64aa6a103aff7005aab8d96058
csvaishakh <vaishakhsubramonian@gmail.com>
2026-03-14 12:21:56 +0530

    feat(database): remove dotenv configuration and clean up imports

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,11 +1,5 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema.js";
-import * as dotenv from "dotenv";
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-dotenv.config({ path: join(__dirname, "../.env") });
 
 if (!process.env["DATABASE_URL"]) {
     throw new Error("DATABASE_URL environment variable is not set");
